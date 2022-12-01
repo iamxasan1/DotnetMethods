7a28ce9c4cf4dca3a8f018be2d452fff02d81947
Xasan <iamxasann@gmail.com>
2022-12-01 20:55:20 +0500

    xexesalom2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,110 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region xexesalom
+// void SalomDunyo()
+// {
+//     Console.WriteLine("Assalomu alaikum hurmatli mehmonlar");
+//     Console.WriteLine("tuzumisila??");
+// }
+// void SalomOzbekiston()
+// {
+//     Console.WriteLine("Assalomu alaikum Oz`bekiston");
+//     Console.WriteLine("tuzumisila??");
+// }
+// void SalomIlmHub()
+// {
+//     Console.WriteLine("Assalomu alaikum IlmHub talabalari");
+//     Console.WriteLine("tuzumisila??");
+// }
+
+// string salomlashishTurlari = Console.ReadLine();
+// switch(salomlashishTurlari)
+// {
+//     case "Ozbekiston":
+//         SalomOzbekiston();
+//         break;
+//     case "Dunyo":
+//         SalomDunyo();
+//         break;
+//     case "IlmHub":
+//         SalomIlmHub();
+//         break;
+//     default:
+//         Console.WriteLine("Assalomu alaikum qadrli dostim");
+//         break;
+
+// }
+#endregion
+
+#region 
+// void PrintRandomNumbers()
+// {
+//     Random random = new Random();
+//     int number1 = random.Next(1, 10);
+//     int number2 = random.Next(1, 100);
+//     int number3 = random.Next(1, 1000);
+//     Console.WriteLine($"{number1},{number2},{number3}");
+// }
+
+// PrintRandomNumbers();
+#endregion
+
+#region string conversation
+
+// static void StringConversation()
+// {
+//     var testString = "xeXeSALOm";
+//     var toLower = testString.ToLower();
+//     System.Console.WriteLine(toLower);
+//     System.Console.WriteLine(testString.ToUpper());
+// }
+
+// StringConversation();
+
+#endregion
+
+#region 
+// void StringArray()
+// {
+//     string testString = "xexesalom";
+//     for(int i = 0; i < testString.Length; i++)
+//     {
+//         System.Console.WriteLine(testString[i]);
+//     }
+// }
+// StringArray();
+#endregion
+
+#region 
+// void PrintNumber(int number)
+// {
+//     System.Console.WriteLine($"you entered: {number}");
+// }
+
+// Console.WriteLine("son kiriting");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// PrintNumber(a);
+#endregion
+
+#region 
+
+void Sum(int x, int y)
+{
+    var result = x + y;
+    System.Console.WriteLine($"{x} + {y} = {result}");
+}
+System.Console.WriteLine("iltimos 1 chi sonni kiriting!!");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("iltimos 2 chi sonni kiriting!!");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Sum(a, b);
+#endregion
+
+
+
+
+
+
+
+
